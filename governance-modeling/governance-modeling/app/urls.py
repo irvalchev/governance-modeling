b3ccbe0bf46f6b967e8a3c1d0be871f518001555
irvalchev <irvalchev@gmail.com>
2017-01-22 11:32:43 +0100

    updated html/css/js files and structure

--- governance-modeling/governance-modeling/app/urls.py
+++ governance-modeling/governance-modeling/app/urls.py
@@ -6,5 +6,4 @@ Definition of urls for the app.
 from django.conf.urls import  url
 import app.views
 
-urlpatterns = [url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about', app.views.about, name='about')]
+urlpatterns = [url(r'^test', app.views.test, name='test')]
